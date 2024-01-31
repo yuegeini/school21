09e6409cc98d69db188c9f13e662c9ea4735cf2b
Onnia Juan <onniajua@at-h3.msk.21-school.ru>
2024-01-31 20:09:59 +0300

    new dec

---
+++ dec/git.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+
+echo "${RED}Write git branch name${NC}"
+read git_branch
+
+git add . > /dev/null 2>&1
+git commit -m "${dt}" > /dev/null 2>&1
+git push origin $git_branch > /dev/null 2>&1
+
+echo "${GREEN}⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀${NC}⠀⠀⠀⠀⠀              +===========================================+"
+echo "${GREEN}⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀${NC}⠀⠀⠀⠀⠀⠀              |Fucked up writing four functions for a git?|"
+echo "${GREEN}⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀${NC}⠀⠀⠀⠀⠀⠀              |            Let me help you.               |"
+echo "${GREEN}⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀${NC}⠀⠀⠀⠀⠀⠀              |    ${RED}Commit name: ${dt}${NC}       |"
+echo "${GREEN}⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆${NC}              |   I'm all done, good luck fucking slave   |"
+echo "${GREEN}⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿${NC}              +===========================================+"
+echo "${GREEN}⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀${NC}⠀⠀⠀⠀               \\    /"
+echo "${GREEN}⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀${NC}⠀⠀⠀⠀                \\  /"
+echo "${GREEN}⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀${NC}⠀⠀⠀⠀                 \\/"
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀${NC}⠀⠀⠀ "
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀${NC}⠀⠀ "
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀${NC}⠀⠀ "
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀${NC}⠀⠀ "
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀${NC}⠀ "
+echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉${NC}"
